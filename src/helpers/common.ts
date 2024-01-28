a3eb29e906c311ced845a59f4a3e2c1048cd1fbe
Varvara Moiseenko <moiseencoo@gmail.com>
2024-01-28 20:06:18 +0500

    fix translation errors

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -1,5 +1,5 @@
 export const sanitizeString = (str: string): string => {
-	return str.replaceAll(/[.,?—]/g,'').toLowerCase()
+	return str.replaceAll(/[.,?—!:;]/g,'').toLowerCase()
 }
 
 export const isObjectEmpty = (objectName: unknown) => {
